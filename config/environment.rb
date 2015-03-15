fa8cae5acf7953fb644a7fe2c1e7da972e051a3a
yenyue <yenyue_07876@yahoo.com>
2015-03-15 20:47:04 +0000

    removed sendgrid and use gmail instead

--- config/environment.rb
+++ config/environment.rb
@@ -4,18 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-#  :address        => 'smtp.gmail.com',
-  :port           => '587',
-  :authentication => :plain,
-#  :user_name      => ENV['SENDGRID_USERNAME'],
-#  :password       => ENV['SENDGRID_PASSWORD'],
-  :user_name            => 'Aapp34639861@heroku.com',
-  :password             => 'qop3kdm4',
-#  :user_name            => 'yenyuepai@gmail.com',
-#  :password             => 'par5ley',
-
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,18 @@
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address              => 'smtp.sendgrid.net',
-  :port                 => '587',
-  :authentication       => :plain,
-  :user_name            => 'Aapp34639861@heroku.com',
-  :password             => 'qop3kdm4',
-  :domain               => 'heroku.com',
+#  :address        => 'smtp.sendgrid.net',
+  :address        => 'smtp.gmail.com',
+  :port           => '587',
+  :authentication => :plain,
+#  :user_name      => ENV['SENDGRID_USERNAME'],
+#  :password       => ENV['SENDGRID_PASSWORD'],
+#  :user_name            => 'Aapp34639861@heroku.com',
+#  :password             => 'qop3kdm4',
+#  :user_name            => 'yenyue_07876@yahoo.com',
+#  :password             => 'par5ley',
+
+  :user_name            => 'yenyuepai@gmail.com',
+  :password             => 'par5ley',
+
+  :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
